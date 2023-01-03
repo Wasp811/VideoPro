82efcb0ea7624d07b184ccf4c559f6b4ce7f1cd4
Wasp <trofa811@gmail.com>
2023-01-03 21:45:27 +0300

    First commit - Unit_19

--- app/src/main/java/com/trofa811/videopro/MainActivity.kt
+++ app/src/main/java/com/trofa811/videopro/MainActivity.kt
@@ -2,10 +2,14 @@ package com.trofa811.videopro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View.inflate
+//import android.view.ViewGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
