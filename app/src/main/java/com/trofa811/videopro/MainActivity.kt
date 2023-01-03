6a047c1437fe21ac18e76b4364f963411626dfc3
Wasp <trofa811@gmail.com>
2023-01-03 21:37:50 +0300

    Initial commit

---
+++ app/src/main/java/com/trofa811/videopro/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.trofa811.videopro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
