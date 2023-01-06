ab84ba1107e8d5fe42b3cae93cc7b20fe994ce63
Wasp <trofa811@gmail.com>
2023-01-06 13:12:21 +0300

    Create first page, menu button (click handler).

--- app/src/main/java/com/trofa811/videopro/MainActivity.kt
+++ app/src/main/java/com/trofa811/videopro/MainActivity.kt
@@ -2,14 +2,41 @@ package com.trofa811.videopro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import android.view.LayoutInflater
-//import android.view.View.inflate
-//import android.view.ViewGroup
+import android.widget.Toast
+import com.trofa811.videopro.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        initButtons()
+    }
+
+
+    private fun initButtons() {
+        button1.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        button2.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        button3.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        button4.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+        button5.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
