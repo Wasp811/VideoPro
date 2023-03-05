bbed12369a979c1e64cd08c497df589fa304cafe
Wasp <trofa811@gmail.com>
2023-03-05 17:55:18 +0300

    Refractoring View in MaterialDesign Add MaterialToolbar Button in Toolbar

--- app/src/main/java/com/trofa811/videopro/MainActivity.kt
+++ app/src/main/java/com/trofa811/videopro/MainActivity.kt
@@ -1,42 +1,56 @@
 package com.trofa811.videopro
 
+//import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.view.ActionMode
+//import android.view.Menu
 import android.widget.Toast
-import com.trofa811.videopro.databinding.ActivityMainBinding
+//import com.google.android.material.bottomnavigation.BottomNavigationView
+//import com.trofa811.videopro.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_main)
 
-        initButtons()
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        bottom_navigation.setOnNavigationItemSelectedListener{
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 
 
-    private fun initButtons() {
-        button1.setOnClickListener {
-            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
-        }
-        button2.setOnClickListener {
-            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-        }
-        button3.setOnClickListener {
-            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
-        }
-        button4.setOnClickListener {
-            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-        }
-        button5.setOnClickListener {
-            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-        }
-
-    }
 }
